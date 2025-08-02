de994e5be0023ae86484a5fd3c41a8c9881d47c2
giselleS08 <giselle.sarabia8@gmail.com>
2025-08-02 23:04:20 +0000

    horchata in the making

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+//creation of sprites
+
+///////////////////////////
+
+
+
+//Drinks
+let horchataText = `
+  bbb`;
+
+  let palette1 = {
+    a: color(51,56,99),//dark
+
+    s:color(118,188,239),//cyanlike
+    d:color(86,149,234),//neutral blue
+    f:color(55,96,193)//gray/dark blue
+  };
+
+      horchata = new Sprite();
+        horchata.img = spriteArt(horchataText, 12, palette1);
+       horchata.pos={x:115, y: 75};
+        horchata.rotation =0;
+        horchata.collider = "kinematic";
+
+
+
+/////////////
+
+
+
+
+//////////////////////////
+
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(800, 800);
   background(133);
 }
 
