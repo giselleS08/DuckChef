fd78bd51215bdb83c820dc9041dce58518f5cfff
giselleS08 <giselle.sarabia8@gmail.com>
2025-08-03 22:56:21 +0000

    palette problem fixed

--- script.js
+++ script.js
@@ -20,14 +20,14 @@ let horchataText = `
   bbb`;
 
   let palette1 = {
-    a: color(51,56,99),//dark
+    b: color(51,56,99),//dark
 
     s:color(118,188,239),//cyanlike
     d:color(86,149,234),//neutral blue
     f:color(55,96,193)//gray/dark blue
   };
         horchata = new Sprite();
-        horchata.img = spriteArt(horchataText, 12, palette1);
+        horchata.img = spriteArt(horchataText, 20, palette1);
        horchata.pos={x:115, y: 75};
         horchata.rotation =0;
         horchata.collider = "kinematic";
